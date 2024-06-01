bf06dbf46b8c0853815da5f3c26480893c313dfc
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-05-31 19:36:29 -0600

    fix problem

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Typewriter
+        words={['Hello, this is a typewriter effect!']}
+        loop={true}
+      />
     </>
   )
 }
